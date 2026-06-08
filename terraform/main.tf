b3275d616618ee9b2ab33828f21597b68c501a2f
Yavuz Akyol <61031463+yavuzakyol20@users.noreply.github.com>
2026-06-08 23:44:44 +0200

    some files has been perfekt with you

--- terraform/main.tf
+++ terraform/main.tf
@@ -23,3 +23,19 @@ resource "azurerm_resource_group" "main" {
     managed_by  = "terraform"
   }
 }
+
+resource "azurerm_storage_account" "lakehouse" {
+  name                     = "oslomobilitylakehouse"
+  resource_group_name      = azurerm_resource_group.main.name
+  location                 = azurerm_resource_group.main.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  account_kind             = "StorageV2"
+  is_hns_enabled           = true
+
+  tags = {
+    project     = "oslo-mobility-lakehouse"
+    environment = "dev"
+    managed_by  = "terraform"
+  }
+}
