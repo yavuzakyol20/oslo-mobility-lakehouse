e6cdfa007d13704668518abf0adcf2cdbac2c4e9
Yavuz Akyol <61031463+yavuzakyol20@users.noreply.github.com>
2026-06-16 22:51:33 +0200

    Add Azure Container Registry infrastructure

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,8 +11,9 @@ terraform {
 
 provider "azurerm" {
   features {}
-}
 
+  storage_use_azuread = true
+}
 resource "azurerm_resource_group" "main" {
   name     = "rg-oslo-mobility-lakehouse"
   location = "norwayeast"
@@ -34,6 +35,20 @@ resource "azurerm_storage_account" "lakehouse" {
   is_hns_enabled                  = true
   allow_nested_items_to_be_public = false
   min_tls_version                 = "TLS1_0"
+
+  tags = {
+    project     = "oslo-mobility-lakehouse"
+    environment = "dev"
+    managed_by  = "terraform"
+  }
+}
+
+resource "azurerm_container_registry" "main" {
+  name                = "acroslomobilitylakehouse"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  sku                 = "Basic"
+  admin_enabled       = false
 
   tags = {
     project     = "oslo-mobility-lakehouse"
